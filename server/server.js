fcfa0006bb7d153c03be69f82a335f72c8ec4a4b
Christian Koch <christiank1030@gmail.com>
2022-10-19 14:42:33 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('client'))
+
+const { getHTML } = require('./controller')
+
+app.get('/', getHTML)
+
+
+
+
+const port = process.env.PORT || 4000
+
+app.listen(port, console.log(`Server running on ${port}`))
